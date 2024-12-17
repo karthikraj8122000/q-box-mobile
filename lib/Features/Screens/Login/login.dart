2ac3cc5ff7918525232b1b121abff2c5ea972f73
karthikrajsk <karthikraj@8122000.com>
2024-12-18 03:18:16 +0530

    login animation

--- lib/Features/Screens/Login/login.dart
+++ lib/Features/Screens/Login/login.dart
@@ -16,11 +16,13 @@ class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin{
   final _formKey = GlobalKey<FormState>();
 
   late TextEditingController _emailController;
   late TextEditingController _passwordController;
+  AnimationController? _animationController;
+  Animation<double>? _animation;
 
   @override
   void initState() {
@@ -34,6 +36,13 @@ class _LoginPageState extends State<LoginPage> {
     _passwordController.addListener((){
       formProvider.setPassword(_passwordController.text);
     });
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 3),
+    );
+    _animation =
+        CurvedAnimation(parent: _animationController!, curve: Curves.easeInOut);
+    _animationController!.forward();
   }
 
   @override
@@ -48,158 +57,161 @@ class _LoginPageState extends State<LoginPage> {
     var obsecureText = loginProvider.obsecureText;
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
+      body: FadeTransition(
+        opacity: _animation!,
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
 
-                const SizedBox(height: 20),
-                const SizedBox(height: 20),
-                const AppText(
-                  text: "Sign In",
-                  fontSize: 24,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.white,
-                ),
-                const AppText(text: "SavorEase – Your Food, Your Way!", fontSize: 14,color: Colors.white70,),
+                  const SizedBox(height: 20),
+                  const SizedBox(height: 20),
+                  const AppText(
+                    text: "Sign In",
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
+                  const AppText(text: "SavorEase – Your Food, Your Way!", fontSize: 14,color: Colors.white70,),
 
-                Form(
-                  key: _formKey,
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      children: [
-                        const Align(
-                            alignment: Alignment.topLeft,
-                            child: AppText(text: "Email Id", fontSize: 18,color: Colors.white38,)),
-                        const SizedBox(height: 10),
-                        CustomTextFormField(
-                          controller: _emailController,
-                          hintText: "Enter email id",
-                          validationRules: [Validator.required(message: "Email is required"), Validator.email()],
-                          suffixIcon: IconButton(
-                              onPressed: () {}, icon: const Icon(Icons.email_outlined,color: Colors.grey,)),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        const Align(
-                            alignment: Alignment.topLeft,
-                            child: AppText(text: "Password", fontSize: 18,color: Colors.white38)),
-                        const SizedBox(height: 10),
-                        CustomTextFormField(
-                          controller: _passwordController,
-                          hintText: "Enter password",
-                          validationRules: [Validator.required(message: "Password is required"), Validator.password()],
-                          suffixIcon: IconButton(
-                            onPressed: () =>
-                                context.read<LoginProvider>().toggleObsecureText(),
-                            icon: Icon(obsecureText
-                                ? Icons.remove_red_eye_outlined
-                                : Icons.visibility_off_outlined,color: Colors.grey),
+                  Form(
+                    key: _formKey,
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          const Align(
+                              alignment: Alignment.topLeft,
+                              child: AppText(text: "Email Id", fontSize: 18,color: Colors.white38,)),
+                          const SizedBox(height: 10),
+                          CustomTextFormField(
+                            controller: _emailController,
+                            hintText: "Enter email id",
+                            validationRules: [Validator.required(message: "Email is required"), Validator.email()],
+                            suffixIcon: IconButton(
+                                onPressed: () {}, icon: const Icon(Icons.email_outlined,color: Colors.grey,)),
                           ),
-                          obscureText: obsecureText,
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                Switch(
-                                  onChanged: (value) {
-                                    loginProvider.toggleRememberMe(value);
-                                  },
-                                  value: loginProvider.rememberMe,
-                                  activeColor: Colors.white,
-                                  activeTrackColor: Theme.of(context).disabledColor,
-                                  inactiveThumbColor: Colors.transparent,
-                                  inactiveTrackColor: Colors.transparent,
-                                  trackOutlineColor:  MaterialStateProperty.all(Colors.white38),
-                                ),
-                                GestureDetector(
-                                  onTap: () {
-                                    loginProvider.toggleRememberMe(!loginProvider.rememberMe);
-                                  },
-                                  child: const AppText(
-                                    text: "Remember me",
-                                    fontSize: 14,
-                                    color: Colors.white38,
-                                  ),
-                                ),
-                              ],
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const Align(
+                              alignment: Alignment.topLeft,
+                              child: AppText(text: "Password", fontSize: 18,color: Colors.white38)),
+                          const SizedBox(height: 10),
+                          CustomTextFormField(
+                            controller: _passwordController,
+                            hintText: "Enter password",
+                            validationRules: [Validator.required(message: "Password is required"), Validator.password()],
+                            suffixIcon: IconButton(
+                              onPressed: () =>
+                                  context.read<LoginProvider>().toggleObsecureText(),
+                              icon: Icon(obsecureText
+                                  ? Icons.remove_red_eye_outlined
+                                  : Icons.visibility_off_outlined,color: Colors.grey),
                             ),
-                            InkWell(
-                              onTap: () {
-
-                              },
-                              child: const Padding(
-                                padding: EdgeInsets.symmetric(vertical: 16),
-                                child: AppText(
-                                  text: "Forgot password?",
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w300,
-                                  color: Colors.white70,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        SizedBox(
-                            width: MediaQuery.of(context).size.width,
-                            height: 50,
-                            child: CustomButton(
-                              elevation: 0,
-                              label: "Login",
-                              color: Theme.of(context).disabledColor,
-                              onPressed: () {
-                                if (_formKey.currentState!.validate()) {
-                                  loginProvider.login(context);
-                                }
-                              },
-                            )),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 16),
-                          child:
+                            obscureText: obsecureText,
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
                           Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              const Padding(
-                                padding: EdgeInsets.only(right: 8.0),
-                                child: AppText(
-                                  text: "Don\'t have an account?",
-                                  fontSize: 14,
-                                  color: Colors.white70,
-                                  fontWeight: FontWeight.w300,
-                                ),
+                              Row(
+                                children: [
+                                  Switch(
+                                    onChanged: (value) {
+                                      loginProvider.toggleRememberMe(value);
+                                    },
+                                    value: loginProvider.rememberMe,
+                                    activeColor: Colors.white,
+                                    activeTrackColor: Theme.of(context).disabledColor,
+                                    inactiveThumbColor: Colors.transparent,
+                                    inactiveTrackColor: Colors.transparent,
+                                    trackOutlineColor:  MaterialStateProperty.all(Colors.white38),
+                                  ),
+                                  GestureDetector(
+                                    onTap: () {
+                                      loginProvider.toggleRememberMe(!loginProvider.rememberMe);
+                                    },
+                                    child: const AppText(
+                                      text: "Remember me",
+                                      fontSize: 14,
+                                      color: Colors.white38,
+                                    ),
+                                  ),
+                                ],
                               ),
                               InkWell(
-                                child: const AppText(
-                                    text: "Sign up",
-                                    fontSize: 14,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w500),
                                 onTap: () {
 
                                 },
-                              )
+                                child: const Padding(
+                                  padding: EdgeInsets.symmetric(vertical: 16),
+                                  child: AppText(
+                                    text: "Forgot password?",
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w300,
+                                    color: Colors.white70,
+                                  ),
+                                ),
+                              ),
                             ],
                           ),
-                        ),
-                      ],
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          SizedBox(
+                              width: MediaQuery.of(context).size.width,
+                              height: 50,
+                              child: CustomButton(
+                                elevation: 0,
+                                label: "Login",
+                                color: Theme.of(context).disabledColor,
+                                onPressed: () {
+                                  if (_formKey.currentState!.validate()) {
+                                    loginProvider.login(context);
+                                  }
+                                },
+                              )),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 16),
+                            child:
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Padding(
+                                  padding: EdgeInsets.only(right: 8.0),
+                                  child: AppText(
+                                    text: "Don\'t have an account?",
+                                    fontSize: 14,
+                                    color: Colors.white70,
+                                    fontWeight: FontWeight.w300,
+                                  ),
+                                ),
+                                InkWell(
+                                  child: const AppText(
+                                      text: "Sign up",
+                                      fontSize: 14,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w500),
+                                  onTap: () {
+
+                                  },
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
--- lib/Features/Screens/Splash/splash_screen.dart
+++ lib/Features/Screens/Splash/splash_screen.dart
@@ -1,120 +1,73 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:lottie/lottie.dart';
 import 'package:qr_page/Features/Screens/Login/login.dart';
+
 
 class SplashScreen extends StatefulWidget {
   static const String routeName = '/splash';
 
   const SplashScreen({super.key});
   @override
-  _SplashScreenState createState() => _SplashScreenState();
+  State<SplashScreen> createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen>
     with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<double> _scaleAnimation;
-  late Animation<double> _fadeAnimation;
+  late AnimationController _controller;
+  late Animation<double> _animation;
 
   @override
   void initState() {
     super.initState();
-
-    // Animation Controller
-    _animationController = AnimationController(
-      duration: const Duration(seconds: 2),
+    _controller = AnimationController(
       vsync: this,
+      duration: const Duration(seconds: 4),
     );
 
-    // Scale Animation (Bounce Effect)
-    _scaleAnimation = Tween<double>(begin: 0.5, end: 1.0).animate(
-      CurvedAnimation(
-        parent: _animationController,
-        curve: Curves.elasticOut,
-      ),
-    );
+    _animation = Tween<double>(begin: 0, end: 1).animate(_controller);
+    _controller.forward();
 
-    // Fade Animation
-    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
-      CurvedAnimation(
-        parent: _animationController,
-        curve: Curves.easeInOut,
-      ),
-    );
-
-    // Navigate to Main Screen
-    _animationController.addStatusListener((status) {
+    _animation.addStatusListener((status) async {
       if (status == AnimationStatus.completed) {
-        Future.delayed(Duration(seconds: 2), () {
-          GoRouter.of(context).push(LoginPage.routeName);
-        });
+        setState(() {});
+        Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginPage(),));
       }
     });
-    // Start the animation
-    _animationController.forward();
   }
 
   @override
   void dispose() {
-    _animationController.dispose();
+    _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Animated Logo with Scale and Fade
-            FadeTransition(
-              opacity: _fadeAnimation,
-              child: ScaleTransition(
-                scale: _scaleAnimation,
-                child: Column(
-                  children: [
-                    // Lottie Animation for Food/Refrigerator Concept
-                    Lottie.asset(
-                      'assets/splash.json', // You'll need to add this Lottie file
-                      width: 300,
-                      height: 300,
-                      fit: BoxFit.fill,
-                    ),
-
-                    SizedBox(height: 20),
-
-                    // App Title with Animated Text
-                    AnimatedBuilder(
-                      animation: _animationController,
-                      builder: (context, child) {
-                        return Opacity(
-                          opacity: _fadeAnimation.value,
-                          child: Transform.translate(
-                            offset: Offset(0, 50 * (1 - _fadeAnimation.value)),
-                            child: Text(
-                              'Food Retention Tracker',
-                              style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                                color: Theme.of(context).primaryColor,
-                              ),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ],
-                ),
+        child: FadeTransition(
+          opacity: _animation,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Lottie.asset(
+                'assets/splash.json', // You'll need to add this Lottie file
+                width: 300,
+                height: 300,
+                fit: BoxFit.fill,
               ),
-            ),
-            SizedBox(height: 40),
-          ],
+              Text(
+                'Food Retention Tracker',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).primaryColor,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
